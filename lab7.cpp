54845bc9c36213c4a13c213d727b4b8b4680e509
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-18 07:55:10 +0000

    Lab-7-Zadanie-1

--- lab7.cpp
+++ lab7.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
+
+/* Zadanie 1 */
+
+void zadanie1()
+{
+    std::cout << "Podaj liczbe elementow wektora" << std::endl;
+    int n;
+    std::cin >> n;
+    try {
+        std::vector< int > wektor(n, 0);
+    }
+    catch (...) {
+        std::cout << "Zostal rzucony wyjatek" << std::endl;
+    }
+}
 
 int main()
 {
-    puts("Hello, World!");
+    zadanie1();
 }
+
+/* Po uruchomieniu otrzymano:
+  Podaj liczbe elementow wektora
+  -2
+  Zostal rzucony wyjatek
+*/
