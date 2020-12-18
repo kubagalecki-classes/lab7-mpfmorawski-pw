c038ac45688307bee80d0446ac8466d94e8ae67f
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-12-18 08:13:00 +0000

    Lab-7-Zadanie-3

--- lab7.cpp
+++ lab7.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <variant>
 #include <vector>
 
 /* Zadanie 1 */
@@ -16,13 +17,56 @@ void zadanie1()
     }
 }
 
+/* Zadanie 2 */
+
+void zadanie2()
+{
+    std::cout << "Podaj liczbe elementow wektora" << std::endl;
+    int n;
+    std::cin >> n;
+    try {
+        std::vector< int > wektor(n, 0);
+    }
+    catch (const std::bad_alloc& ba) {
+        std::cout << ba.what() << std::endl;
+    }
+}
+
+/* Zadanie 3 */
+
+void zadanie3()
+{
+    int a, b;
+    std::cout << "Podaj liczbe a" << std::endl;
+    std::cin >> a;
+    std::cout << "Podaj liczbe b" << std::endl;
+    std::cin >> b;
+    try {
+        std::vector< int >                      wektor(a, 0);
+        std::variant< int, float, std::string > wariant;
+        if (b % 2 == 0)
+            wariant = 42;
+        else
+            wariant = "nieparzyste";
+        std::cout << std::get< int >(wariant) << std::endl;
+    }
+    catch (const std::bad_alloc& ba) {
+        std::cout << ba.what() << std::endl;
+    }
+    catch (const std::bad_variant_access& bva) {
+        std::cout << bva.what() << std::endl;
+    }
+}
+
 int main()
 {
-    zadanie1();
+    zadanie3();
 }
 
 /* Po uruchomieniu otrzymano:
-  Podaj liczbe elementow wektora
-  -2
-  Zostal rzucony wyjatek
+  Podaj liczbe a
+  10
+  Podaj liczbe b
+  3
+  Unexpected index
 */
